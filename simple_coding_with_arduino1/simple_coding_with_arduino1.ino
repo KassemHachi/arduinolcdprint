83f5765152789b3752817f9f1a5a8dd62cc1100e
KassemHachi <kassem.hachi@outlook.com>
2019-05-28 07:44:58 -0700

    print kassemhachi in lcd

---
+++ simple_coding_with_arduino1/simple_coding_with_arduino1.ino
@@ -0,0 +1,18 @@
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("KassemHachi");
+
+}
+
+void loop() {
+  
+  lcd.setCursor(0, 1);
+  
+  
+}
+ 
